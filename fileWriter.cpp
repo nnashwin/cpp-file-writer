fbf4c093d78a056c31c1565b0ba92b8595e04fa3
ttymed <tyler.boright@gmail.com>
2017-11-18 13:24:49 -0800

    changes the implementation of the writeFile and adds a stub for reading

--- fileWriter.cpp
+++ fileWriter.cpp
@@ -1,3 +1,19 @@
+#include "fileWriter.h"
+#include <fstream>
+#include <string>
+
+int writeFile (char textStr[], std::string filePath) {
+	std::ofstream myfile(filePath, std::ios::binary);
+	myfile.write((char *) &textStr, sizeof(&textStr));
+	return 0;
+}
+
+std::string readFile (std::string filePath) {
+	return "cowabunga";
+}
+
 int main () {
+	char copyMe[] = "There is no cow level ajsdkfjaskdfjksdajfksadjfkasdjfksadjfksdajkfasdjkfsadjkfjasdkfjdsakfjsdkafjsdakfjdueueueueu";
+	writeFile(copyMe, "./cows.test");
 	return 0;
 }
