6a50e727be1dcf0947949facee4c0b8af483cd86
ttymed <tyler.boright@gmail.com>
2017-11-18 22:43:24 -0800

    makes changes to fileWriter to make it write and read plain strings (for now)

--- fileWriter.cpp
+++ fileWriter.cpp
@@ -1,19 +1,35 @@
 #include "fileWriter.h"
+#include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
-int writeFile (char textStr[], std::string filePath) {
-	std::ofstream myfile(filePath, std::ios::binary);
-	myfile.write((char *) &textStr, sizeof(&textStr));
-	return 0;
+void writeFile (char textStr[], std::string filePath) {
+	std::ofstream myfile(filePath);
+	myfile << textStr;
+	myfile.close();
 }
 
 std::string readFile (std::string filePath) {
-	return "cowabunga";
+	std::string line;
+	std::string output;
+	std::ifstream myfile(filePath, std::ios::binary);
+	if (myfile.is_open()) {
+		while (getline(myfile, line)) {
+			output += line;
+			output += '\n';
+		}
+		myfile.close();
+	} else std::cout << "You are unable to open the file";
+
+	return output;
 }
 
 int main () {
-	char copyMe[] = "There is no cow level ajsdkfjaskdfjksdajfksadjfkasdjfksadjfksdajkfasdjkfsadjkfjasdkfjdsakfjsdkafjsdakfjdueueueueu";
+	char copyMe[] = "cookies There is no cow level ajsdkfjaskdfjksdajfksadjfkasdjfksadjfksdajkfasdjkfsadjkfjasdkfjdsakfjsdkafjsdakfjdueueueueu";
 	writeFile(copyMe, "./cows.test");
+	std::string fileResult = readFile("./cows.test");
+	std::cout << fileResult << std::endl;
+	
 	return 0;
 }
